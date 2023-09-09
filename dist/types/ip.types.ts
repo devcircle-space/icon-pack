5f5b4d6fcb8040035e019b2f575f48140e34aa4e
Aakash Jha <aakash.jha1103@gmail.com>
2023-09-09 19:56:46 +0530

    Adding new icons - info and close

--- dist/types/ip.types.ts
+++ dist/types/ip.types.ts
@@ -1,21 +1,33 @@
 export type IconPackId =
   | "fi-arrow-left"
   | "fi-arrow-right"
-  | "fi-chevron-down";
+  | "fi-chevron-down"
+  | "fi-info"
+  | "fi-x-circle"
+  | "fi-x";
 
 export type IconPackKey =
   | "FiArrowLeft"
   | "FiArrowRight"
-  | "FiChevronDown";
+  | "FiChevronDown"
+  | "FiInfo"
+  | "FiXCircle"
+  | "FiX";
 
 export enum IconPack {
   FiArrowLeft = "fi-arrow-left",
   FiArrowRight = "fi-arrow-right",
   FiChevronDown = "fi-chevron-down",
+  FiInfo = "fi-info",
+  FiXCircle = "fi-x-circle",
+  FiX = "fi-x",
 }
 
 export const ICON_PACK_CODEPOINTS: { [key in IconPack]: string } = {
   [IconPack.FiArrowLeft]: "61697",
   [IconPack.FiArrowRight]: "61698",
   [IconPack.FiChevronDown]: "61699",
+  [IconPack.FiInfo]: "61700",
+  [IconPack.FiXCircle]: "61701",
+  [IconPack.FiX]: "61702",
 };
